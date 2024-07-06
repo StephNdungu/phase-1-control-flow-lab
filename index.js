5251903ca54191744fd61d42150ff8c6d9b6bfe2
StephNdungu <stephy.wanjiku@gmail.com>
2024-07-06 21:11:22 +0300

    Completes lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(rideFeet){
+  let result;
+  if (rideFeet <= 400 ){
+    return ('This one is on me!')
+  }
+  else if (
+    rideFeet > 400 && rideFeet <=2000){
+return ('That will be twenty bucks.');
+  }
+  else if (
+    rideFeet > 2000){
+      return ('I will gladly take your thirty bucks.');
+    }
+  else if (
+    rideFeet > 2500){
+      return ('No can do');
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result2 ;
+  if (city === 'NYC'){
+    return ('Ok sounds good!')
+  }
+else {
+  return ('No go')
 }
+let city = Pittsburgh
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tipResponse){
+  switch (tipResponse){
+    case "generous":
+      return "Thank you so much."
+  
+  case "not as generous":
+    return "Thank you."
+
+default: return "Bye."
+  }
 }
